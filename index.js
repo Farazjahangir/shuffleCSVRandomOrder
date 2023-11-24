962bda88cd673866e75e980c3157e15f590c6021
Farazjahangir <jahangirfaraz98@gmail.com>
2023-11-24 19:12:28 +0500

    shuffle and write logic change

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const csv = require('csv-parser');
+// const csv = require('csv-parser');
+const csv = require("fast-csv");
+const _ = require('lodash')
+
 
 // Function to shuffle array elements randomly
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+  return _.shuffle(array)
 }
 
 // Function to read CSV file, shuffle rows, and save to a new CSV file
@@ -15,22 +15,23 @@ function shuffleCSV(inputFile, outputFile) {
 
   // Read CSV file
   fs.createReadStream(inputFile)
-    .pipe(csv())
+    .pipe(csv.parse({ headers: true }))
     .on('data', (row) => {
       rows.push(row);
     })
     .on('end', () => {
       // Shuffle rows
-      shuffleArray(rows);
-
-      // Write to new CSV file
+      const shuffledData = shuffleArray(rows);
       const ws = fs.createWriteStream(outputFile);
-      ws.write(Object.keys(rows[0]).join(',') + '\n'); // Write header
-      rows.forEach((row) => {
-        ws.write(Object.values(row).join(',') + '\n');
-      });
-
-      console.log('CSV file shuffled and saved successfully!');
+      csv
+        .write(shuffledData, { headers: true })
+        .pipe(ws)
+        .on("finish", function () {
+          console.log("CSV file shuffled and saved successfully!");
+        })
+        .on("error", function (err) {
+          console.error("Error merging data and exporting to CSV file:", err);
+        });
     });
 }
 
